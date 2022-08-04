6b478d5c9e4ea8eda4e571d06cbe8c97d09d2a6d
Hunter Ducharme <hgducharme@gmail.com>
2022-08-04 00:35:46 -0500

    Added system info bar to polybar

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -10,6 +10,7 @@ while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 # Launch each bar
 echo "----" | tee -a /tmp/workspaces.log /tmp/info.log
 polybar info 2>&1 | tee -a /tmp/info.log & disown
+polybar system 2>&1 | tee -a /tmp/system.log & disown
 polybar workspaces 2>&1 | tee -a /tmp/workspaces.log & disown
 # polybar apps 2>&1 | tee -a /tmp/apps.log & disown
 
