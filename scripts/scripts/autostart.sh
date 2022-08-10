c47337b393013c442f336a3acbe46e2a98d73140
Hunter Ducharme <hgducharme@gmail.com>
2022-08-10 13:16:04 -0500

    removed .profile and created autostart.sh

--- scripts/scripts/autostart.sh
+++ scripts/scripts/autostart.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
+
 # MPD daemon start (if no other user instance exists)
 [ ! -s ~/.config/mpd/pid ] && mpd
 
 # Launch polybar with the colors of the specified wallpaper
-bash ~/.config/polybar/custom/scripts/pywal.sh ~/Documents/Wallpapers/nordic-wallpapers/wallpapers/at_the_coffeeshop.png
+bash ~/.config/polybar/custom/scripts/pywal.sh ~/repos/nordic-wallpapers/wallpapers/at_the_coffeeshop.png
 bash ~/.config/polybar/launch.sh --custom
